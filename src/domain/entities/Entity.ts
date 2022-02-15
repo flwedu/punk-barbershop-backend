3faa2a6ab68c53e1ea233516fa90084aa9d8e655
Eduardo Aquino <edutraquino@gmail.com>
2022-02-15 20:01:47 -0300

    getId() method add

--- src/domain/entities/Entity.ts
+++ src/domain/entities/Entity.ts
@@ -1,19 +1,14 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
 export abstract class Entity<T> {
-
     private _id: string;
-
-    protected get id(): string {
-        return this._id;
-    }
-    protected set id(value: string) {
-        this._id = value;
-    }
     public props: T;
 
     constructor(props: T, id?: string) {
         this.props = props;
-        this.id = id ?? crypto.randomUUID();
+        this._id = id ?? crypto.randomUUID();
+    }
+    get id(): string {
+        return this._id;
     }
 }
