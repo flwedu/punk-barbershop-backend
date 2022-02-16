61b74439c044b2f12c30ae8eb02399a269a2bcc9
Eduardo Aquino <edutraquino@gmail.com>
2022-02-16 19:42:17 -0300

    create index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { createServer } from 'http'
+
+const server = createServer((request, response) => {
+    response.writeHead(200, "Ok", { contentType: 'text/html' })
+    response.write("<p>Sucess! The application is running</p>")
+})
+
+server.listen(8080, () => console.log("server running at 8080"))
