97c03cc9dc7865884bea403daec2fbe5321e5399
Eduardo Aquino <edutraquino@gmail.com>
2022-02-18 19:52:11 -0300

    create valueObject

---
+++ src/domain/valueObjects/Cpf.ts
@@ -0,0 +1,37 @@
+import { testCpf } from "../../util/testFields";
+import { ValueObject } from "./ValueObject";
+
+export class Cpf implements ValueObject<Cpf>{
+
+    private _value: string;
+
+    private constructor(value: string) {
+        this._value = value.replace(/[\.\-]/g, '');
+    }
+
+    static of(value: string): Cpf{
+        if(!testCpf(value)){
+            throw new Error("Invalid CPF")
+        }
+        return new Cpf(value);
+    }
+
+    isValid(){
+        return testCpf(this._value);
+    }
+
+    getValue(){
+        return this._value;
+    }
+
+    getFormatedValue(): string{
+
+        const exp = /^(\d{3})(\d{3})(\d{3})(\d{2})$/
+        return this._value.replace(exp, "$1.$2.$3-$4")
+    }
+
+    equals(other: Cpf): boolean {
+        return this._value === other.getValue();
+    }
+
+}
