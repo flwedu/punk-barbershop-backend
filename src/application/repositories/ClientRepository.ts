8cc578db78e168b9e1501329f9a384c905e6d5b5
Eduardo Aquino <edutraquino@gmail.com>
2022-02-18 22:00:56 -0300

    add save method to repository

--- src/application/repositories/ClientRepository.ts
+++ src/application/repositories/ClientRepository.ts
@@ -1,6 +1,8 @@
-import { Client } from "../../domain/entities/client";
+import { Client, ClientProps } from "../../domain/entities/client";
 
 export interface ClientRepository {
 
-    findById(id: string): Promise<Client | null>;
+    findById(id: string): Promise<Client>;
+
+    save(props: ClientProps, id?: string): Promise<Client>;
 }
