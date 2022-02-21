085b5818e3f99b8d13aa4512d4c9b5c104f8ae33
Eduardo Aquino <edutraquino@gmail.com>
2022-02-21 18:53:42 -0300

    add find abstract method

--- src/application/repositories/IRepository.ts
+++ src/application/repositories/IRepository.ts
@@ -2,6 +2,7 @@ import { Entity, Props } from "../../domain/entities/Entity";
 
 export default abstract class IRepository<T extends Entity>{
 
+    abstract find(query: any): Promise<T[]>
     abstract findById(id: string): Promise<T>;
     abstract findAll(): Promise<T[]>;
     abstract save(props: Props<T>, id?: string): Promise<T>;
