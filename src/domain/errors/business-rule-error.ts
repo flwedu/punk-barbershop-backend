d1a1ae9a2c0aeba38565297b5fa20c28c16e3cbb
Eduardo Aquino <edutraquino@gmail.com>
2022-02-26 20:09:57 -0300

    + error classes

---
+++ src/domain/errors/business-rule-error.ts
@@ -0,0 +1,6 @@
+export default class BusinessRuleError extends Error {
+
+    constructor(message: string) {
+        super(message);
+    }
+}
---
+++ src/domain/errors/resource-not-found.ts
@@ -0,0 +1,6 @@
+export default class ResourceNotFount extends Error {
+
+    constructor(message: string) {
+        super(message);
+    }
+}
