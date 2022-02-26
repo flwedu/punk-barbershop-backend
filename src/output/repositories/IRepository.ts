2fba28cc374e56c5f4bc51e9ee1edcbf2ab8c002
Eduardo Aquino <edutraquino@gmail.com>
2022-02-26 19:01:37 -0300

    repository implementations should receive entities

--- src/output/repositories/IRepository.ts
+++ src/output/repositories/IRepository.ts
@@ -1,11 +1,11 @@
-import { Entity, Props } from "../../domain/entities/Entity";
+import { Entity } from "../../domain/entities/Entity";
 
 export default abstract class IRepository<T extends Entity>{
 
     abstract find(query: any): Promise<T[]>
     abstract findById(id: string): Promise<T>;
     abstract findAll(): Promise<T[]>;
-    abstract save(props: Props<T>, id?: string): Promise<T>;
-    abstract update(props: Props<T>, id: string): Promise<T>;
+    abstract save(entity: T, id?: string): Promise<T>;
+    abstract update(entity: T, id: string): Promise<T>;
     abstract delete(id: string): Promise<any>;
 }
