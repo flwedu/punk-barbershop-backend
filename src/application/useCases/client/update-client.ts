34403a2e910b95c1d1c176ee918a67277068c10d
Eduardo Aquino <edutraquino@gmail.com>
2022-02-26 19:31:03 -0300

    + use case

---
+++ src/application/useCases/client/update-client.ts
@@ -0,0 +1,20 @@
+import { Client, InputClientProps } from "../../../domain/entities/client";
+import IRepository from "../../../output/repositories/IRepository";
+import IUseCase from "../IUseCase";
+
+type UpdateRequest = {
+    id: string,
+    props: InputClientProps
+}
+
+export default class UpdateClientUseCase implements IUseCase {
+
+    constructor(private readonly repository: IRepository<Client>) { }
+
+    async execute(data: UpdateRequest) {
+
+        await this.repository.findById(data.id);
+        const client = Client.create(data.props, data.id)
+        return this.repository.update(client, data.id);
+    }
+}
