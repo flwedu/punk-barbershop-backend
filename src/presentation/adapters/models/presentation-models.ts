458a4c74aa64f97f0feaabf27594ec583112f043
Eduardo Aquino <edutraquino@gmail.com>
2022-02-27 14:02:18 -0300

    + presentation models for entities

---
+++ src/presentation/adapters/models/presentation-models.ts
@@ -0,0 +1,6 @@
+import { InputBarberProps } from "../../../domain/entities/barber";
+import { InputClientProps } from "../../../domain/entities/client"
+
+export type ClientPresentationModel = InputClientProps;
+
+export type BarberPresentationModel = InputBarberProps;
