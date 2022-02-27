f37a9a4c5e296ba1e1f866f4a1c68fe6772480e6
Eduardo Aquino <edutraquino@gmail.com>
2022-02-27 14:01:33 -0300

    + create client controller class and tests

---
+++ src/presentation/controllers/client/create-client-controller.spec.ts
@@ -0,0 +1,66 @@
+import { Client } from "../../../domain/entities/client";
+import { IMRepository } from "../../../output/repositories/test/IM-Repository";
+import CreateClientController from "./create-client-controller";
+
+describe("create client controller", () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    function setup() {
+        const repository = new IMRepository<Client>();
+        const sut = new CreateClientController(repository);
+
+        return { repository, sut };
+    }
+
+    it("should get a response entity with 201 status code", async () => {
+
+        expect.assertions(2);
+
+        const { repository, sut } = setup();
+        const response = await sut.handle({
+            name: "Test",
+            cpf: "12345678911",
+            birthDate: "2020-01-01",
+            email: "test@email.com",
+        })
+
+        expect(response.status).toEqual(201);
+        expect(repository.list.length).toEqual(1);
+    })
+
+    it("should get a response entity with 400 status code", async () => {
+
+        expect.assertions(4);
+
+        const { repository, sut } = setup();
+        const response = await sut.handle({
+            name: "Test",
+            cpf: "",
+            birthDate: "2020-01-01",
+            email: "test@email.com",
+        })
+
+        const response2 = await sut.handle({
+            name: "Test",
+            cpf: "12345678911",
+            birthDate: "2020-01-01",
+            email: "",
+        })
+
+        const response3 = await sut.handle({
+            name: "Test",
+            cpf: "12345678911",
+            birthDate: "",
+            email: "test@email.com",
+        })
+
+        expect(response.status).toEqual(400);
+        expect(response2.status).toEqual(400);
+        expect(response3.status).toEqual(400);
+        expect(repository.list.length).toEqual(0);
+    })
+
+})
---
+++ src/presentation/controllers/client/create-client-controller.ts
@@ -0,0 +1,24 @@
+import { CreateClientUseCase } from "../../../application/useCases/client/create-client";
+import { Client, InputClientProps } from "../../../domain/entities/client";
+import IRepository from "../../../output/repositories/IRepository";
+import { ClientPresentationModelAdapter } from "../../adapters/client-presentation-model-adapter";
+import { createResponseEntityForError } from "../../http/response-entity-functions";
+import Controller from "../Controller";
+
+export default class CreateClientController implements Controller {
+
+    constructor(private readonly repository: IRepository<Client>) { };
+
+    async handle(data: InputClientProps) {
+        try {
+            const client = await new CreateClientUseCase(this.repository).execute(data);
+            const response = new ClientPresentationModelAdapter().toModel(client);
+            return {
+                status: 201,
+                data: response
+            }
+        } catch (err) {
+            return createResponseEntityForError(err);
+        }
+    }
+}
