f20522a4cb7815075a0657203242c81afdc6177b
Eduardo Aquino <edutraquino@gmail.com>
2022-02-26 22:41:13 -0300

    + builder functions to http response entities

---
+++ src/presentation/http/response-entity-functions.ts
@@ -0,0 +1,40 @@
+import BusinessRuleError from "../../domain/errors/business-rule-error";
+import ResourceNotFound from "../../domain/errors/resource-not-found";
+import IResponseEntity from "./ResponseEntity";
+
+export function createOkResponse<T>(data: T): IResponseEntity<T> {
+    return {
+        status: 200,
+        data
+    }
+}
+
+export function createResponseEntityForError(error: Error) {
+    if (error instanceof ResourceNotFound) {
+        return createNotFoundResponse(error.message);
+    }
+    if (error instanceof BusinessRuleError) {
+        return createBadRequestErrorResponse(error.message);
+    }
+}
+
+export function createNotFoundResponse(data: any): IResponseEntity<any> {
+    return {
+        status: 400,
+        data
+    }
+}
+
+export function createServerErrorResponse(data: any): IResponseEntity<any> {
+    return {
+        status: 500,
+        data
+    }
+}
+
+export function createBadRequestErrorResponse(data: any): IResponseEntity<any> {
+    return {
+        status: 400,
+        data
+    }
+}
