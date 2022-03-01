3a6b616c28905a8ef572756ba7e53df0231ed339
Eduardo Aquino <edutraquino@gmail.com>
2022-03-01 16:32:20 -0300

    + tests

--- src/application/useCases/client/update-client.spec.ts
+++ src/application/useCases/client/update-client.spec.ts
@@ -1,7 +1,39 @@
+import { IMRepository } from "../../../output/repositories/test/IM-Repository"
+import { Client } from "../../domain/entities/client"
+import UpdateClientUseCase from "./update-client";
+
 describe("Update client use case", () => {
 
-    it.skip("Sould update a client", async () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
 
+    it("Sould return the update a client", async () => {
 
+        expect.assertions(3);
+        const repository = new IMRepository<Client>();
+        const sut = new UpdateClientUseCase(repository);
+        const repositorySpy = jest.spyOn(repository, "update");
+
+        const client = Client.create({
+            name: "Test",
+            email: "aa@email.com",
+            birthDate: "01/01/2021",
+            cpf: "00000000000"
+        }, "1");
+        repository.list.push(client)
+
+        expect(repository.list.at(0).props.name).toEqual("Test");
+        sut.execute({
+            id: "1",
+            props: {
+                name: "Updated test",
+                email: "aa@email.com",
+                birthDate: "01/01/2021",
+                cpf: "00000000000"
+            }
+        })
+        expect(repositorySpy).toBeCalledTimes(1);
+        expect(repository.list.at(0).props.name).toEqual("Updated test");
     })
 })
