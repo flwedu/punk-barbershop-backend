8a07b6185c280ad80f2e2aa218065df814a0b28f
Eduardo Aquino <edutraquino@gmail.com>
2022-03-05 15:16:35 -0300

    router passes request and response to controller

--- src/main/routes/clientExpressRouter.ts
+++ src/main/routes/clientExpressRouter.ts
@@ -1,26 +1,20 @@
 import { Router } from "express";
+import { Client } from "../../application/domain/entities";
 import IRepository from "../../output/repositories/IRepository";
 import { FindAllController, FindByIdController } from "../../presentation/controllers";
 import { CreateClientController } from "../../presentation/controllers/client";
 
 export default function configureClientRoutes(router: Router, repository: IRepository<any>){
 
-    router.get("/clients", function (_, res) {
-        new FindAllController(repository).handle().then(
-            result => res.status(result.status).send(result.data)
-        ).catch(err => res.send(err));
+    router.get("/clients", (request, response) => {
+        new FindAllController<Client>(repository).handle(request, response);
     })
     
-    router.get("/clients/:id", function (req, res) {
-        const { id } = req.params
-        new FindByIdController(repository).handle({ id }).then(
-            result => res.status(result.status).send(result.data)
-        ).catch(err => res.send(err));
+    router.get("/clients/:id", (request, res) => {
+        new FindByIdController<Client>(repository).handle(request, response);
     })
     
-    router.post("/clients", function (req, res) {
-        new CreateClientController(repository).handle({ props: req.body }).then(
-            result => res.status(result.status).send(result.data)
-        ).catch(err => res.send(err));
+    router.post("/clients", (request, response) => {
+       new CreateClientController(repository).handle(request, response);
     })
 }
