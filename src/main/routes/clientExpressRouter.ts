f0d284b17029f8f4721350bbe9423f4d035f3b73
Eduardo Aquino <edutraquino@gmail.com>
2022-03-05 16:23:55 -0300

    renaming param res to response

--- src/main/routes/clientExpressRouter.ts
+++ src/main/routes/clientExpressRouter.ts
@@ -4,17 +4,17 @@ import IRepository from "../../output/repositories/IRepository";
 import { FindAllController, FindByIdController } from "../../presentation/controllers";
 import { CreateClientController } from "../../presentation/controllers/client";
 
-export default function configureClientRoutes(router: Router, repository: IRepository<any>){
+export default function configureClientRoutes(router: Router, repository: IRepository<any>) {
 
     router.get("/clients", (request, response) => {
         new FindAllController<Client>(repository).handle(request, response);
     })
-    
-    router.get("/clients/:id", (request, res) => {
+
+    router.get("/clients/:id", (request, response) => {
         new FindByIdController<Client>(repository).handle(request, response);
     })
-    
+
     router.post("/clients", (request, response) => {
-       new CreateClientController(repository).handle(request, response);
+        new CreateClientController(repository).handle(request, response);
     })
 }
