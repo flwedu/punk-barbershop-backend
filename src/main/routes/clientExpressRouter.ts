28f6ed3b3d8c6c4b3bea67edbda27b2d85ecbe57
Eduardo Aquino <edutraquino@gmail.com>
2022-03-05 09:54:37 -0300

    receiving repository in constructor

--- src/main/routes/clientExpressRouter.ts
+++ src/main/routes/clientExpressRouter.ts
@@ -1,28 +1,25 @@
 import { Router } from "express";
-import { clientRepository } from "../../main/config/resourcesFactory";
-import CreateClientController from "../../presentation/controllers/client/create-client-controller";
-import { FindAllController } from "../../presentation/controllers/findall-controller";
-import FindByIdController from "../../presentation/controllers/findby-id-controller";
+import IRepository from "output/repositories/IRepository";
+import { FindAllController, FindByIdController } from "presentation/controllers";
+import { CreateClientController } from "presentation/controllers/client";
 
-
-
-export default function configureClientRoutes(router: Router){
+export default function configureClientRoutes(router: Router, repository: IRepository<any>){
 
     router.get("/clients", function (_, res) {
-        new FindAllController(clientRepository).handle().then(
+        new FindAllController(repository).handle().then(
             result => res.status(result.status).send(result.data)
         ).catch(err => res.send(err));
     })
     
     router.get("/clients/:id", function (req, res) {
         const { id } = req.params
-        new FindByIdController(clientRepository).handle({ id }).then(
+        new FindByIdController(repository).handle({ id }).then(
             result => res.status(result.status).send(result.data)
         ).catch(err => res.send(err));
     })
     
     router.post("/clients", function (req, res) {
-        new CreateClientController(clientRepository).handle({ props: req.body }).then(
+        new CreateClientController(repository).handle({ props: req.body }).then(
             result => res.status(result.status).send(result.data)
         ).catch(err => res.send(err));
     })
