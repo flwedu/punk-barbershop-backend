1dc8f75528d4571c9ddd368e848f56e99c0fe123
Eduardo Aquino <edutraquino@gmail.com>
2022-03-04 22:14:35 -0300

    updating import of Config

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,4 +1,6 @@
-import { server } from './config/config';
+import { Config } from "./config/config";
 
+const server = Config.server;
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => { console.log(`Server is running at: http://localhost:${PORT}/`) });
--- src/main/routes/default.ts
+++ src/main/routes/default.ts
@@ -1,3 +1,5 @@
-import { router } from "../config/config";
+import { Config } from "../config/config"
+
+const { router } = Config;
 
 router.get("/", (req, res) => res.send("API is working!").status(200))
