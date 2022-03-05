8f1dabb9f41fbf17b32040cdfffd2c3ab125cfb1
Eduardo Aquino <edutraquino@gmail.com>
2022-03-05 18:08:44 -0300

    extracting props from request.body

--- src/presentation/controllers/client/create-client-controller.ts
+++ src/presentation/controllers/client/create-client-controller.ts
@@ -10,9 +10,9 @@ export default class CreateClientController implements Controller {
 
     constructor(private readonly repository: IRepository<Client>) { };
 
-    async handle(request: Request, response: Response) {
+    async handle(request, response) {
         try {
-            const client = await new CreateClientUseCase(this.repository).execute(request.body);
+            const client = await new CreateClientUseCase(this.repository).execute({ props: request.body });
             const result = new EntityModelParser().toModel(client);
             return new ResponseFactory(response).responseWithDifferentCode(201, result);
         } catch (err) {
