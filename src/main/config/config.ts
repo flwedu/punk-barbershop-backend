7eec53ee2db28becdc49941818dc423fe8fd48da
Eduardo Aquino <edutraquino@gmail.com>
2022-03-06 12:24:07 -0300

    fix undefined req body: use(json()) before router

--- src/main/config/config.ts
+++ src/main/config/config.ts
@@ -1,16 +1,15 @@
+import cors from "cors";
+import express from "express";
 import { Barber, Client, Scheduling, ServiceType } from "../../application/domain/entities";
 import { IMRepository } from "../../output/repositories/test/IM-Repository";
 import configureClientRoutes from "../routes/clientExpressRouter";
-import { json } from "body-parser";
-import cors from "cors"
-import express from "express";
 
 // Configuring server and routes
 const server = express();
 const router = express.Router();
 
+server.use(express.json());
 server.use("/api", router);
-server.use(json());
 server.use(cors());
 
 // Configuring repositories
