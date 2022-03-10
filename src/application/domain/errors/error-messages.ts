a6f6b6022a9e68018bf8d37eaf0100e56162a889
Eduardo Aquino <edutraquino@gmail.com>
2022-03-10 20:04:25 -0300

    add param to errorMessages

--- src/application/domain/errors/error-messages.ts
+++ src/application/domain/errors/error-messages.ts
@@ -1,5 +1,5 @@
 export const ErrorMessage = {
-    ID_NOT_FOUND: (id: string) => `Element with id ${id} not found`,
-    ID_ALREADY_EXISTS: (id: string) => `Element with id ${id} already exists`,
-    INVALID_PARAM: (param: string) => `Invalid ${param}`
+    ID_NOT_FOUND: (id: string, name: string = "Element") => `${name} with id ${id} not found`,
+    ID_ALREADY_EXISTS: (id: string, name: string = "Element") => `${name} with id ${id} already exists`,
+    INVALID_PARAM: (param: string) => `Invalid ${param}`,
 }
