6d2b5941cdb52a27a2f862615fada02adb824423
Eduardo Aquino <edutraquino@gmail.com>
2022-03-10 20:05:20 -0300

    validating date

--- src/util/parser.ts
+++ src/util/parser.ts
@@ -1,9 +1,10 @@
+import { ErrorMessage } from "../application/domain/errors/error-messages";
 import BusinessRuleError from "../application/domain/errors/business-rule-error";
 
 export function parseDateValue(value: string) {
     const date = Date.parse(value);
-    if (!date) {
-        throw new BusinessRuleError("Could not convert date")
+    if (!value || !date) {
+        throw new BusinessRuleError(ErrorMessage.INVALID_PARAM("date"))
     }
     return new Date(date);
 }
