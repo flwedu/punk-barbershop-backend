d07d2e429138c2cf5bd006648b274efc374c4d91
Eduardo Aquino <edutraquino@gmail.com>
2022-03-25 18:15:37 -0300

    app get server from app config class

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,7 +1,7 @@
-import { Config } from "./config/config";
 import "dotenv/config"
+import { defaultAppConfig } from "./config/config";
 
-const server = Config.server;
+const server = defaultAppConfig.getServer();
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => { console.log(`Server is running at: http://localhost:${PORT}/`) });
