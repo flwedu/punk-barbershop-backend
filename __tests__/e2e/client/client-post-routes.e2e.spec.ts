3ea64c904607a1bc59f682a1341d38e93a237bd0
Eduardo Aquino <edutraquino@gmail.com>
2022-03-26 07:02:43 -0300

    using testInMemoryAppConfiguration

--- __tests__/e2e/client/client-post-routes.e2e.spec.ts
+++ __tests__/e2e/client/client-post-routes.e2e.spec.ts
@@ -2,32 +2,33 @@ import faker from "@faker-js/faker";
 import supertest from "supertest";
 import { Client } from "../../../src/application/domain/entities";
 import { ErrorMessage } from "../../../src/application/domain/errors/error-messages";
-import { Config } from "../../../src/main/config/config";
+import { testInMemoryAppConfiguration } from "../../../src/main/config/test-configuration";
 import { IMRepository } from "../../../src/output/repositories/test/IM-Repository";
 import EntityModelParser from "../../../src/presentation/adapters/entity-model-parser";
 import { createFakeClientProps } from "../../../src/__test_utils__/MockDataFactory"
 
 describe("Tests for Client #POST controller controller", () => {
 
-    const server = Config.server;
-    const repository = Config.repositories.clientRepository;
     const parser = new EntityModelParser();
+    const app = testInMemoryAppConfiguration;
+    const repository = app.getRepository("Client");
 
     beforeEach(() => {
-        Config.repositories.clientRepository = new IMRepository<Client>();
+        app.setRepository("Client", new IMRepository<Client>());
     })
 
-    test("Should return 201 and body should contains the data about created client", async () => {
+    test("Should return 201 and body should contains the id of created client", async () => {
 
         const requestData = {
+            id: "1",
             ...createFakeClientProps()
         }
-        const response = await supertest(server).post("/api/clients").send(requestData);
+        const response = await supertest(app.getServer()).post("/api/clients").send(requestData);
         const data = JSON.parse(response.text);
 
         expect(response.statusCode).toEqual(201);
-        expect(data).toEqual(parser.toModel(repository.list.at(0)));
-        expect(data).toMatchObject({ ...requestData });
+        expect(data).toEqual(requestData.id);
+        expect(data).toEqual(expect.any(String));
     })
 
     test.each([[{
@@ -47,7 +48,7 @@ describe("Tests for Client #POST controller controller", () => {
     }, ErrorMessage.INVALID_PARAM("CPF")]
     ])("Should return 400 and body is $1", async (requestData, message) => {
 
-        const response = await supertest(server).post("/api/clients").send(requestData);
+        const response = await supertest(app.getServer()).post("/api/clients").send(requestData);
         const data = JSON.parse(response.text);
 
         expect(response.statusCode).toEqual(400);
--- src/main/app.ts
+++ src/main/app.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
-import { defaultAppConfig } from "./config/config";
+import { defaultAppConfiguration } from "./config/default-configuration";
 
-const server = defaultAppConfig.getServer();
+const server = defaultAppConfiguration.getServer();
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => { console.log(`Server is running at: http://localhost:${PORT}/`) });
