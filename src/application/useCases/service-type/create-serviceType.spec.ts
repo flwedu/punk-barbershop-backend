8fe4545ff6c6ebb4ed905127e904825ba1e9a549
Eduardo Aquino <edutraquino@gmail.com>
2022-03-30 19:52:59 -0300

    test: add creation tests w/out id and refactoring

--- src/application/useCases/service-type/create-serviceType.spec.ts
+++ src/application/useCases/service-type/create-serviceType.spec.ts
@@ -1,7 +1,8 @@
+import crypto from "crypto";
 import { ErrorMessage } from "../../../application/domain/errors/error-messages";
 import { createFakeServiceTypeProps } from "../../../__test_utils__/MockDataFactory";
 import { setupRepository } from "../../../__test_utils__/setupFunctions";
-import { ServiceType } from "../../domain/entities/serviceType";
+import { InputServiceTypeProps, ServiceType } from "../../domain/entities/serviceType";
 import { CreateServiceTypeUseCase } from "./create-serviceType";
 
 describe("create serviceType use case test", () => {
@@ -10,20 +11,33 @@ describe("create serviceType use case test", () => {
         jest.clearAllMocks();
     })
 
-    it("Should create a serviceType", async () => {
+    test.each([createFakeServiceTypeProps()])("Should create a serviceType without passing an id", async (props: InputServiceTypeProps) => {
 
         expect.assertions(3);
         const { repository, repositorySpy } = setupRepository<ServiceType>("save");
         const sut = new CreateServiceTypeUseCase(repository);
 
-        const resultId = await sut.execute(createFakeServiceTypeProps());
+        const resultId = await sut.execute({ props });
 
         expect(resultId).toEqual(expect.any(String));
         expect(await repository.findById(resultId)).toMatchObject({ ...ServiceType });
         expect(repositorySpy).toHaveBeenCalledTimes(1);
     })
 
-    it.each(["", "abc1", "0", null, "-1"])("Should throw an error when trying to create a serviceType with this invalid duration value: %s", async (duration) => {
+    test.each([crypto.randomUUID(), "1", "1121"])("Should create a serviceType passing an id", async (id: string) => {
+
+        expect.assertions(3);
+        const { repository, repositorySpy } = setupRepository<ServiceType>("save");
+        const sut = new CreateServiceTypeUseCase(repository);
+
+        const resultId = await sut.execute({ id, props: createFakeServiceTypeProps() });
+
+        expect(resultId).toEqual(id);
+        expect(await repository.findById(resultId)).toMatchObject({ ...ServiceType });
+        expect(repositorySpy).toHaveBeenCalledTimes(1);
+    })
+
+    test.each(["", "abc1", "0", null, "-1"])("Should throw an error when trying to create a serviceType with this invalid duration value: %s", async (duration) => {
 
         expect.assertions(2);
         const { repository, repositorySpy } = setupRepository<ServiceType>("save");
@@ -31,8 +45,10 @@ describe("create serviceType use case test", () => {
 
         try {
             await sut.execute({
-                ...createFakeServiceTypeProps(),
-                duration,
+                props: {
+                    ...createFakeServiceTypeProps(),
+                    duration,
+                }
             });
 
         } catch (err) {
@@ -49,8 +65,10 @@ describe("create serviceType use case test", () => {
 
         try {
             await sut.execute({
-                ...createFakeServiceTypeProps(),
-                price,
+                props: {
+                    ...createFakeServiceTypeProps(),
+                    price,
+                }
             });
         } catch (err) {
             expect(err.message).toEqual(ErrorMessage.INVALID_PARAM("price"))
